d4e27fe4627da90e7d13ba1e9895aae8f083dd96
Eduardo Moises <eduardo.moisesqa@hotmail.com>
2023-06-23 12:46:26 -0300

    first commit

---
+++ minhasfinacas/minhasfinacas/src/test/java/com/edumoises/minhasfinacas/MinhasfinacasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edumoises.minhasfinacas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhasfinacasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
